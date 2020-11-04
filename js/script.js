65513a0a0045984f2d802eed8866c6e4d4af7133
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-04 18:11:37 +0100

    versione beta

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
 
+  // finzione per creare dropdown menù che viene fuori al click
   $('.dropitem').click(function () {
 
+    $('.dropdown').removeClass("active"); // se aperto, chiudi l'altro
+
     $(this).find('.dropdown').toggleClass('active');
+
   });
 
 });
