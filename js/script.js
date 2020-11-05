d5448b263e6ea62cfb4a354314259222917313d8
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-05 15:52:24 +0100

    aggiunto commento

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
   // funzione dropdown
   $(document).ready(function(){
     $(".dropitem").click(function(){
-      if ($(this).find(".dropdown").is(":visible")) {
-        $(".dropdown").hide();
+      if ($(this).find(".dropdown").is(":visible")) { // se aperto, chiudi l'altro
+        $(".dropdown").hide(); 
       } else {
         $(".dropdown").hide();
         $(this).find(".dropdown").toggle();
